607fdab37561de0def64275495a7b8610c904861
newDev911 <ars.urock77@gmaail.com>
2022-05-30 00:30:45 +0500

    Class -2 Starting

---
+++ DerekBanas_Class2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DerekBans_CSharp_Class_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
